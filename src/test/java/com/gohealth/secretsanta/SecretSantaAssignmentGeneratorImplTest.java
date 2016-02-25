c7f87275c9cc2f5f36bb8cdf64051361c6eb6c9b
Jeremy Curran <jcurran24@gmail.com>
2016-02-25 12:40:22 -0600

    Removed commented out code no longer needed.

--- src/test/java/com/gohealth/secretsanta/SecretSantaAssignmentGeneratorImplTest.java
+++ src/test/java/com/gohealth/secretsanta/SecretSantaAssignmentGeneratorImplTest.java
@@ -15,14 +15,6 @@ public class SecretSantaAssignmentGeneratorImplTest {
 	private final String[] participants = new String[] { "Kyle", "Kenny", "Eric", "Stan", "Stewie", "Brian" };
 	private final String[] oddParticipants = new String[] { "Kyle", "Kenny", "Eric", "Stan", "Stewie" };
 	
-	/*
-	public SecretSantaAssignmentGeneratorImplTest() {
-		this.assignmentGenerator = new SecretSantaAssignmentGeneratorImpl();
-	}
-	
-	public SecretSantaAssignmentGeneratorImplTest(SecretSantaAssignmentGenerator assignmentGenerator) {
-		this.assignmentGenerator = assignmentGenerator;
-	}*/
 	
 	@Test
 	public void testGenerateAssignmentsParticipantsNotNull() {
